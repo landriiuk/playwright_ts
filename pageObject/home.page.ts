05364cb4e487b21734dd69b86d3b8546bd66e3f5
landriiuk <andriukluba@gmail.com>
2024-11-02 14:33:18 +0200

    add details to test 1

--- pageObject/home.page.ts
+++ pageObject/home.page.ts
@@ -4,11 +4,15 @@ export class HomePage {
   readonly page: Page;
   readonly getLogo: Locator;
   readonly getHomeItemMenuBar: Locator;
+  readonly getNavBarItems: Locator;
+  readonly getTopMenuNavBarItems: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.getLogo = this.page.getByTitle("Automation Test Store");
     this.getHomeItemMenuBar = this.page.locator(".menu_home");
+    this.getNavBarItems = this.page.locator(".main_menu");
+    this.getTopMenuNavBarItems = this.getNavBarItems.locator(".top");
   }
 
   async navigateToPage(url: string = process.env.BASE_UR || "") {
@@ -16,5 +20,27 @@ export class HomePage {
   }
   async verifyUrl(url: string = process.env.BASE_UR || "") {
     await expect(this.page).toHaveURL(url);
+  }
+
+  async verifyLogoIsVisible() {
+    await expect(this.getLogo).toBeVisible();
+  }
+
+  async verifyMainMenuNavigationItems() {
+    const mainMenuItems = {
+    '':''
+    }
+    for (let i = 0; i < (await this.getNavBarItems.count()); i++) {
+      // if (await this.getTopMenuNavBarItems.isVisible()) {
+      for (let i = 0; i < (await this.getTopMenuNavBarItems.count()); i++) {
+        expect(this.getTopMenuNavBarItems.nth(i).innerText()).toBe(mainMenuItems[i]);
+      }
+
+      //}
+    }
+    // for (let index = 0; index < array.length; index++) {
+    //   const element = array[index];
+
+    // }
   }
 }
--- tests/UI/mainPageItems.spec.ts
+++ tests/UI/mainPageItems.spec.ts
@@ -4,7 +4,40 @@ test.describe("Verify main page", async () => {
   test("UI_TC_001 Verify the home page loads without errors and displays all key elements.", async ({
     homePage,
   }) => {
-    await homePage.navigateToPage();
-    await homePage.verifyUrl();
+    test.step("Navigate to main page", async () => {
+      await homePage.navigateToPage();
+    });
+    test.step("Navigate to", async () => {
+      await homePage.verifyUrl();
+    });
+
+    test.step("Verify the presence of the following key elements", async () => {
+      test.step("Site Logo: Confirm that the logo is visible", async () => {
+        await homePage.verifyLogoIsVisible();
+      });
+
+      test.step("Main Navigation Menu. Check for items: Login or Register, Specials, Account, Cart, Checkout", async () => {
+
+      });
+    });
+
+    test.step("Navigate to", async () => {});
+
+    test.step("Navigate to", async () => {});
+
+    test.step("Navigate to", async () => {});
   });
 });
+// 1. **Open Google Chrome**.
+// 2. **Navigate to** [Automation Test Store](https://automationteststore.com/).
+// 3. **Wait for the page to fully load**.
+// 4. **Verify the presence of the following key elements**:
+//    - **Site Logo**: Confirm that the logo is visible and correctly displayed.
+//    - **Main Navigation Menu**:
+//      - Check for customer top menu items: Login or Register, Specials, Account, Cart, Checkout.
+//      - Verify that these items are clickable and lead to the correct pages.
+//    - **Secondary Navigation Bar**: Ensure visibility and correctness of the following items:
+//      - Home, Apparel & Accessories, Makeup, Skincare, Fragrance, Men, Hair Care, Books.
+// 5. **Footer Section**:
+//    - Verify the presence of footer items: About Us, Contact Us, Testimonials, Newsletter Signup.
+//    - Ensure that links in the footer are functional and direct users to the appropriate pages (About Us, Privacy Policy, Return Policy, Shipping Information, Contact Us, Site Map, Login).
