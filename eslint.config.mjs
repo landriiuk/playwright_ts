62a5a288d26384d2b4673a3a4b4de3b2a524989b
landriiuk <andriukluba@gmail.com>
2024-11-14 17:18:51 +0200

    add gitignore into eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  includeIgnoreFile(gitignorePath),
+
   {
     rules: {
       'quotes': ['error', 'single', { avoidEscape: true }],
